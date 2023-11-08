5325014058aa2c84b107016148b596b1123c06b4
PAROMITA NAG <paromitanagofficial@gmail.com>
2023-11-08 12:47:35 +0530

    initial commit by amit and paromita in lab
    splash, main, and navigation based done by amit

--- app/src/main/java/com/example/advokit/MainActivity.java
+++ app/src/main/java/com/example/advokit/MainActivity.java
@@ -6,15 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button b;
+    ImageView facebook, google;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b = findViewById(R.id.btnLogin);
+        facebook = findViewById(R.id.facebookLogo);
+        google = findViewById(R.id.googlelogo);
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"InBuild",Toast.LENGTH_SHORT);
+            }
+        });
+
+        // google pennding
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this,"InBuild",Toast.LENGTH_SHORT);
+            }
+        });
+
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,4 +48,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 }
